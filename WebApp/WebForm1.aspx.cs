a7529f92d8ecff20289d41598ac773f8005ebf5f
Baldev <Baldev@DESKTOP-TGB9VTP>
2018-09-23 22:16:56 -0500

    Add project files.

---
+++ WebApp/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            CalculatorWebServices.CalculatorWebServiceSoapClient client = new CalculatorWebServices.CalculatorWebServiceSoapClient();
+            int Result = client.Add(Convert.ToInt32(txtFirstNumber.Text), Convert.ToInt32(txtSecondNumber.Text));
+            lblResult.Text = Result.ToString();
+
+            gvRecentCalculations.DataSource = client.GetRecentTransection();
+            gvRecentCalculations.DataBind();
+
+            gvRecentCalculations.HeaderRow.Cells[0].Text = "Recent Calculations";
+
+        }
+    }
+}
---
+++ WebServicesLearningProjects/CalculatorWebService.asmx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServicesLearningProjects
+{
+    /// <summary>
+    /// Summary description for CalculatorWebService
+    /// </summary>
+    [WebService(Namespace = "http://www.baldebbhattarai.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod(EnableSession=true)]
+        public int Add(int firstNumber, int secondNumber)
+        {
+            List<string> calculations;
+
+            if (Session["RecentCalculations"] == null)
+            {
+                calculations = new List<string>();
+            }
+            else
+            {
+                calculations = (List<string>)Session["RecentCalculations"];
+            }
+
+            string strTransaction = firstNumber.ToString() + " + "
+                + secondNumber.ToString()
+                + " = " + (firstNumber + secondNumber).ToString();
+            calculations.Add(strTransaction);
+            Session["RecentCalculations"] = calculations;
+
+            return firstNumber +secondNumber;
+        }
+        [WebMethod(EnableSession = true)]
+        public List<string> GetRecentTransection()
+        {
+            
+            if (Session["RecentCalculations"] == null)
+            {
+                List<string> calculations = new List<string>();
+                calculations.Add("You have not performed any calculations");
+                return calculations;
+            }
+            else
+            {
+                return (List<string>)Session["RecentCalculations"];
+            }
+        }
+    }
+}
