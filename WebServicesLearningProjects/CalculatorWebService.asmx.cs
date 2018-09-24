723742fd19b458a4c6fdd1c14081ce95547ab538
Baldev <Baldev@DESKTOP-TGB9VTP>
2018-09-24 00:02:45 -0500

    WebMethod Overloading using MessageName attribute properties.

--- WebServicesLearningProjects/CalculatorWebService.asmx.cs
+++ WebServicesLearningProjects/CalculatorWebService.asmx.cs
@@ -10,14 +10,40 @@ namespace WebServicesLearningProjects
     /// Summary description for CalculatorWebService
     /// </summary>
     [WebService(Namespace = "http://www.baldebbhattarai.com/")]
-    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [WebServiceBinding(ConformsTo = WsiProfiles.None)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
     public class CalculatorWebService : System.Web.Services.WebService
     {
 
+
+        [WebMethod(MessageName = "Add3Numbers")]
+        //this message name is used as a function within soap body.
+        public int Add(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            List<string> calculations;
+
+            if (Session["RecentCalculations"] == null)
+            {
+                calculations = new List<string>();
+            }
+            else
+            {
+                calculations = (List<string>)Session["RecentCalculations"];
+            }
+
+            string strTransaction = firstNumber.ToString() + " + "
+                + secondNumber.ToString()
+                + " = " + (firstNumber + secondNumber).ToString();
+            calculations.Add(strTransaction);
+            Session["RecentCalculations"] = calculations;
+
+            return firstNumber + secondNumber;
+        }
+
         [WebMethod(EnableSession=true, Description="This method adds two numbers.", CacheDuration = 5)]
+
         public int Add(int firstNumber, int secondNumber)
         {
             List<string> calculations;
@@ -39,6 +65,7 @@ namespace WebServicesLearningProjects
 
             return firstNumber +secondNumber;
         }
+
         [WebMethod(EnableSession = true)]
         public List<string> GetRecentTransection()
         {
